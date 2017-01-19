9825d361acddc32d6efdecb2669987d487fda8f4
Michael Andrade and Tracie Weitzman <tracie@>
2017-01-19 11:45:31 -0800

    refactor edit keg component

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,26 +7,9 @@ import { Keg } from './keg.model';
   <div class="container">
     <h1>Tap Room</h1>
     <keg-list [childKegList]="masterKegList" (clickSender)="editKeg($event)"></keg-list>
-    <div *ngIf="selectedKeg">
-      <h4>{{selectedKeg.name}}</h4>
-      <h4>Edit Keg</h4>
-      <label>Enter Name</label>
-      <input [(ngModel)]="selectedKeg.name">
-      <br>
-      <label>Enter Brewery</label>
-      <input [(ngModel)]="selectedKeg.brand">
-      <br>
-      <label>Enter Price</label>
-      <input [(ngModel)]="selectedKeg.price">
-      <br>
-      <label>Enter Alcohol Content</label>
-      <input [(ngModel)]="selectedKeg.alcoholContent">
-      <br>
-      <label>Enter Beer Style</label>
-      <input [(ngModel)]="selectedKeg.style">
-      <br>
-      <button (click)="finishedEditing()">Done</button>
-    </div>
+    <hr>
+    <edit-keg [childSelectedKeg]="selectedKeg"
+    (doneButtonClickedSender)="finishedEditing()"></edit-keg>
   </div>
   `
 })
---
+++ app/edit-keg.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'edit-keg',
+  template: `
+    <div>
+      <div *ngIf="childSelectedKeg">
+        <h4>{{childSelectedKeg.name}}</h4>
+        <h4>Edit Keg</h4>
+        <label>Enter Name</label>
+        <input [(ngModel)]="childSelectedKeg.name">
+        <br>
+        <label>Enter Brewery</label>
+        <input [(ngModel)]="childSelectedKeg.brand">
+        <br>
+        <label>Enter Price</label>
+        <input [(ngModel)]="childSelectedKeg.price">
+        <br>
+        <label>Enter Alcohol Content</label>
+        <input [(ngModel)]="childSelectedKeg.alcoholContent">
+        <br>
+        <label>Enter Beer Style</label>
+        <input [(ngModel)]="childSelectedKeg.style">
+        <br>
+        <button (click)="doneButtonClicked()">Done</button>
+      </div>
+    </div>
+  `
+})
+
+export class EditKegComponent {
+  @Input() childSelectedKeg: Keg;
+  @Output() doneButtonClickedSender = new EventEmitter();
+
+  doneButtonClicked() {
+    this.doneButtonClickedSender.emit();
+  }
+}
